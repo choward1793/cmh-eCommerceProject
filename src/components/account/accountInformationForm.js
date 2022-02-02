6b7026ba9c29b81b7faa208907e097ca74e79025
choward1793 <choward1793@yahoo.com>
2022-02-02 09:06:57 -0600

    finished account information form

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -63,7 +63,7 @@ class AccountInformationForm extends Component {
                     name='zipcode'
                     component={FormInput}
                 />
-
+                <div className='account-information-form__line'></div>
                 {
                     this.state.showPasswords ?
                         [
@@ -87,6 +87,21 @@ class AccountInformationForm extends Component {
                                 placeholder='Confirm Password'
                                 name='confirm'
                                 component={FormInput}
+                            />,
+                            <Field key={3} className='account-information-form__update-information'
+                                onClick={() => this.setState({ showPasswords: false })}
+                                type='submit'
+                                title='Update Information'
+                                name='update-information'
+                                component={FormButton}
+                            />,
+                            <Field key={4} className='account-information-form__cancel'
+                                onClick={() => this.setState({ showPasswords: false })}
+                                type='button'
+                                title='Cancel'
+                                name='cancel'
+                                short={true}
+                                component={FormButton}
                             />
                         ]
           
